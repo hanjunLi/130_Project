75867cb7dd171bd7829c480ea3cc7d655f5fb972
Hanjun Li <h1212@g.ucla.edu>
2018-02-12 20:47:45 -0800

    added play-list funtion

--- Web/speax/speax/urls.py
+++ Web/speax/speax/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('speaxapp/', include('speaxapp.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Web/speax/speaxapp/views.py
+++ Web/speax/speaxapp/views.py
@@ -13,19 +13,41 @@ from django.core.files import File
 # gtts or other speech engine
 from gtts import gTTS
 
+class track:
+    def __init__(self, name="", path=""):
+        self.name = name
+        self.path = path
+        
+
 def get_audio_from_text(text_file):
+    # real implementation may use other speech engine
+    # temporarily: use gTTS over internet
     text_buffer = text_file.read()
     tts = gTTS(text=text_buffer, lang='en', slow=False)
     return tts
 
+def get_jason_response():
+    # real implementation through RPC
+    # temporarily: read from file
+    pass
+
+def make_tracks(jason_data):
+    
+    pass
+
 
 def simple_upload(request):
+    template_dict = {};
+
+    
     if request.method == 'POST' and 'myfile' in request.FILES:
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
         uploaded_file_url = fs.url(filename)
 
+        template_dict['uploaded_file_url'] = uploaded_file_url
+        template_dict['file_type'] = myfile.content_type
         if myfile.content_type == "text/plain" and not myfile.multiple_chunks():
             # small text/plain type file
             with open(fs.path(filename), 'r') as text:
@@ -34,16 +56,7 @@ def simple_upload(request):
             with open('myaudio', 'w+b') as af:
                 audio.write_to_fp(af)
                 audio_url = fs.url(fs.save(af.name, af))
-                
-            return render(request, 'speaxapp/simple_upload.html', {
-                'uploaded_file_url': uploaded_file_url,
-                'file_type' : myfile.content_type,
-                'audio_path' : audio_url
-            })
 
-        else:
-            return render(request, 'speaxapp/simple_upload.html', {
-                'uploaded_file_url': uploaded_file_url,
-                'file_type' : myfile.content_type
-            })
-    return render(request, 'speaxapp/simple_upload.html')
+            template_dict['tracks'] = [track("uploaded_text", audio_url)]
+
+    return render(request, 'speaxapp/simple_upload.html', template_dict)
