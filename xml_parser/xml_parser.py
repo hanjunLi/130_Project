45e8cdc10018424a5a2d4c32a525289a8c40a077
sjslin <sjslin@gmail.com>
2018-03-02 02:11:17 -0800

    [math], hyphenated words fixed, empty tracks fixed, json decode for testing

--- xml_parser/xml_parser.py
+++ xml_parser/xml_parser.py
@@ -3,7 +3,11 @@
 import xml.etree.ElementTree as ET
 import json
 from collections import OrderedDict
+import os
+
 ns = '{http://www.tei-c.org/ns/1.0}'  # xml namespace
+proj_dir = '/Users/sherrylin/130_Project/'
+
 
 # get title and author from TEI/teiHeader/fileDesc/sourceDesc/biblStruct/analytic
 # input: <analytic> Element
@@ -12,21 +16,21 @@ def get_metadata(analytic):
     metadata = OrderedDict()
     # get title
     try:
-        title = analytic.find(ns+'title').text
+        title = analytic.find(ns + 'title').text
     except AttributeError:
         title = 'No title found'
-    #print('title is', title)
+    # print('title is', title)
     metadata['title'] = title
 
     # get authors
     authors = ""
-    for node in analytic.iter(ns+'persName'):
-        forename = node.find(ns+'forename')
+    for node in analytic.iter(ns + 'persName'):
+        forename = node.find(ns + 'forename')
         try:
             authors += forename.text + ' '
         except AttributeError:
             authors += 'FirstNameUnknown '
-        surname = node.find(ns+'surname')
+        surname = node.find(ns + 'surname')
         try:
             authors += surname.text + ', '
         except AttributeError:
@@ -35,7 +39,7 @@ def get_metadata(analytic):
         authors = "No authors found"
     else:
         authors = authors[:-2]  # remove ', ' from last author
-    #print('authors are', authors)
+    # print('authors are', authors)
     metadata['authors'] = authors
     return metadata
 
@@ -46,21 +50,27 @@ def get_metadata(analytic):
 def get_abstract(abs_elem):
     abs_track = OrderedDict()
     abs_p = ""
+
     for elem in abs_elem.iter():
         tag = elem.tag
-        if tag == ns + 'ref' or tag == ns + 'p':
+        if tag == ns + 'ref':
             abs_p += elem.text + ' '
             if elem.tail is not None:
-                abs_p += elem.tail + ' '
+                if not elem.tail.isspace():  # don't add if tail is just whitespace
+                    abs_p += str.replace(elem.tail, '- ', '') + ' '
+        elif tag == ns + 'p':
+            abs_p += '\n' + str.replace(elem.text, '- ', '')
         elif tag == ns + 'formula':
-            abs_p += '<math> '
+            abs_p += '[math] '
     if abs_p == "":
         abs_p = "No abstract found"
-    else:
-        abs_p = abs_p[:-1]  # remove last space
+    else: # remove last space and first newline
+        if abs_p[0] == '\n':
+            abs_p = abs_p[1:]
+        if abs_p[-1] == ' ':
+            abs_p = abs_p[:-1]
     abs_track["id"] = "abstract"
     abs_track["content"] = abs_p
-    # print(abs_track)
     return abs_track
 
 
@@ -71,9 +81,9 @@ def get_tracks(body):
     tracks = OrderedDict()
     tracknum = 3  # 1 should be "title and authors"; 2 should be "abstract"
 
-    for div in body.iter(ns+'div'):
-        content = ''
-        head = div.find(ns+'head')
+    for div in body.iter(ns + 'div'):
+        content = ""
+        head = div.find(ns + 'head')
         try:
             sec_num = head.attrib['n'] + ' '
         except (KeyError, AttributeError):
@@ -83,18 +93,26 @@ def get_tracks(body):
         except AttributeError:
             sec_title = 'No section title'
         my_id = sec_num + sec_title
-        #print('id is', my_id)
+        # print('id is', my_id)
         for elem in div.iter():  # should handle references and formulas
             tag = elem.tag
-            if tag == ns + 'ref' or tag == ns + 'p':
+            if tag == ns + 'ref':
                 content += elem.text + ' '
-                if elem.tail is not None:  # handles text after <ref>...</ref>
-                    content += elem.tail + ' '
+                if elem.tail is not None:
+                    if not elem.tail.isspace():  # don't add if tail is just whitespace
+                        content += str.replace(elem.tail, '- ', '') + ' '
+            elif tag == ns + 'p':
+                content += '\n' + str.replace(elem.text, '- ', '')
             elif tag == ns + 'formula':
-                content += '<math> '
-        if content != '':
-            content = content[:-1]  # take off last space
-        #print('content is', content)
+                content += '[math] '
+        if content == '':
+            content = "No content found"
+        else:  # remove last space and first newline
+            if content[0] == '\n':
+                content = content[1:]
+            if content[-1] == ' ':
+                content = content[:-1]
+        # print('content is', content)
         this_track = OrderedDict()
         this_track['id'] = my_id
         this_track['content'] = content
@@ -103,33 +121,44 @@ def get_tracks(body):
     return tracks
 
 
+def xml_parser(file_name):
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    output = OrderedDict()
+
+    metadata = get_metadata(
+        root.find(ns + 'teiHeader').find(ns + 'fileDesc').find(ns + 'sourceDesc').find(ns + 'biblStruct').find(
+            ns + 'analytic'))
+    tracks = get_tracks(root.find(ns + 'text').find(ns + 'body'))
+
+    # add "title and authors", "abstract" sections to tracks OrderedDict
+    title_and_authors = OrderedDict()
+    title_and_authors['id'] = 'title and authors'
+    title_and_authors['content'] = metadata['title'] + ' by ' + metadata['authors']
+
+    tracks['1'] = title_and_authors
+    tracks['2'] = get_abstract(root.find(ns + 'teiHeader').find(ns + 'profileDesc').find(ns + 'abstract'))
+    tracks.move_to_end('2', last=False)  # move tracks 1,2 to the beginning
+    tracks.move_to_end('1', last=False)
+    output['metadata'] = metadata
+    output['tracks'] = tracks
+
+    json_str = json.dumps(output, indent=2)
+    return json_str
+
+
 def main():
-  tree = ET.parse('/Users/sherrylin/130_Project/test/test1.pdf.tei.xml')
-  root = tree.getroot()
-  output = OrderedDict()
+    json_str = xml_parser(proj_dir + "/test/4_short.pdf.tei.xml")
+    decoded = bytes(json_str, "utf-8").decode("unicode_escape")
+    print(decoded)
+    f = open(proj_dir + 'test/test.json', 'w+')
+    f.write(decoded)
+    f.close()
+    g = open(proj_dir + 'test/json.json', 'w+')
+    g.write(json_str)
+    g.close()
 
-  metadata = get_metadata(root.find(ns+'teiHeader').find(ns+'fileDesc').find(ns+'sourceDesc').find(ns+'biblStruct').find(ns+'analytic'))
-  tracks = get_tracks(root.find(ns+'text').find(ns+'body'))
 
-  # add "title and authors", "abstract" sections to tracks OrderedDict
-  title_and_authors = OrderedDict()
-  title_and_authors['id'] = 'title and authors'
-  title_and_authors['content'] = metadata['title'] + ' by ' + metadata['authors']
-
-  tracks['1'] = title_and_authors
-  tracks['2'] = get_abstract(root.find(ns+'teiHeader').find(ns+'profileDesc').find(ns+'abstract'))
-  tracks.move_to_end('2', last=False)   # move tracks 1,2 to the beginning
-  tracks.move_to_end('1', last=False)
-  output['metadata'] = metadata
-  output['tracks'] = tracks
-
-  json_str = json.dumps(output, indent=2)
-  print(json_str)
-  with open('/Users/sherrylin/130_Project/test/data.json', 'w') as f:
-      f.write(json_str)
-      f.close()
-
-  
 
 if __name__ == "__main__":
     main()
